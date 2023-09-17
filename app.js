95343d80cd1892dc43dafd8ba3ed43c774f29b78
Samuel Okoye <Samuelokoye3310@gmail.com>
2023-09-17 18:30:54 +0100

    nav-bar

--- app.js
+++ app.js
@@ -3,3 +3,11 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
